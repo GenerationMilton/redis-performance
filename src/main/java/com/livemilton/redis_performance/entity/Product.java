bc122c53ecebb66c628a0a27eed83a9f448a7a0f
Milton Muñoz <livemilton@gmail.com>
2024-10-20 20:26:23 -0500

    data setup service to redis performance

--- src/main/java/com/livemilton/redis_performance/entity/Product.java
+++ src/main/java/com/livemilton/redis_performance/entity/Product.java
@@ -1,12 +1,16 @@
 package com.livemilton.redis_performance.entity;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 import org.springframework.data.annotation.Id;
 
 @Data
 @ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class Product {
 
     @Id
