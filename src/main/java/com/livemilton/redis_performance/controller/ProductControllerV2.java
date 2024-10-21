9d1aec82120eae5927b9a3a75c35a9b9e6d1376c
Milton Muñoz <livemilton@gmail.com>
2024-10-21 08:08:22 -0500

    product service v2 Demo to redis

---
+++ src/main/java/com/livemilton/redis_performance/controller/ProductControllerV2.java
@@ -0,0 +1,31 @@
+package com.livemilton.redis_performance.controller;
+
+import com.livemilton.redis_performance.entity.Product;
+import com.livemilton.redis_performance.service.ProductServiceV2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("product/v2")
+public class ProductControllerV2 {
+
+    @Autowired
+    private ProductServiceV2 service;
+
+    @GetMapping("{id}")
+    public Mono<Product> getProduct(@PathVariable int id){
+        return this.service.getProduct(id);
+    }
+
+    @PutMapping("{id}")
+    public Mono<Product> updateProduct(@PathVariable int id, @RequestBody Mono<Product> productMono){
+        return this.service.updateProduct(id, productMono);
+    }
+
+    @DeleteMapping("{id}")
+    public Mono<Void> deleteProduct(@PathVariable int id){
+        return this.service.deleteProduct(id);
+    }
+
+}
--- src/main/java/com/livemilton/redis_performance/service/util/ProductCacheTemplate.java
+++ src/main/java/com/livemilton/redis_performance/service/util/ProductCacheTemplate.java
@@ -1,4 +1,59 @@
 package com.livemilton.redis_performance.service.util;
 
-public class ProductCacheTemplate {
+import com.livemilton.redis_performance.entity.Product;
+import com.livemilton.redis_performance.repository.ProductRepository;
+import org.redisson.api.RMapReactive;
+import org.redisson.api.RedissonReactiveClient;
+import org.redisson.codec.TypedJsonJacksonCodec;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+public class ProductCacheTemplate extends CacheTemplate<Integer, Product> {
+
+    //DEPENDENCY INJECTION
+    @Autowired
+    private ProductRepository repository;
+    private RMapReactive<Integer,Product> map;
+
+    //CONSTRUCTOR WITH REDISSON
+    public ProductCacheTemplate(RedissonReactiveClient client) {
+
+        this.map = client.getMap("product", new TypedJsonJacksonCodec(Integer.class,Product.class));
+    }
+
+
+    //IMPLEMENTED METHODS
+    @Override
+    protected Mono<Product> getFromSource(Integer id) {
+        return this.repository.findById(id);
+    }
+
+    @Override
+    protected Mono<Product> getFromCache(Integer id) {
+        return this.map.get(id);
+    }
+
+    @Override
+    protected Mono<Product> updateSource(Integer id, Product product) {
+        return this.repository.findById(id)
+                .doOnNext(p->product.setId(id))
+                .flatMap(p->this.repository.save(product));
+    }
+
+    @Override
+    protected Mono<Product> updateCache(Integer id, Product product) {
+        return this.map.fastPut(id,product).thenReturn(product);
+    }
+
+    @Override
+    protected Mono<Void> deleteFromSource(Integer id) {
+        return this.repository.deleteById(id);
+    }
+
+    @Override
+    protected Mono<Void> deleteFromCache(Integer id) {
+        return this.map.fastRemove(id).then();
+    }
 }
